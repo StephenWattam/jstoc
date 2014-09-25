c4ef9e9f55b2e7ff60fc16b2d4d6bf84cdce4ecc
Steve Wattam <stephenwattam@gmail.com>
2014-09-25 20:14:20 +0100

    Added updated jstoc version from T2

--- jstoc.js
+++ jstoc.js
@@ -4,20 +4,20 @@
  * based on the algorithm by
  * Matt Whitlock <http://www.whitsoftdev.com/>
  */
-function generateTOC(toc) {
+function generateTOC(toc, parnt) {
 
   // Min, max levels of heading to notice (inclusive)
-  var toclim  = [2,4],                // limits for TOC (NB: h1=0, h2=1, etc..)
-      headlim = [2,4],                // limits for headings
+  var toclim  = [1,4],                // limits for TOC (NB: h1=0, h2=1, etc..)
+      headlim = [1,4],                // limits for headings
 
-  // Display options
-      heading_nums  = true,           // Display leading numbers in headings?
+      // Display options
+      heading_nums  = false,           // Display leading numbers in headings?
       link_nums     = true,           // Display leading numbers in TOC?
 
-  // Counters
+      // Counters
       rx        = /^h([1-6])$/i,      // Regex for matching headers
       levels    = [0, 0, 0, 0, 0, 0], // TOC depth counters
-      container = "ol",               // Type of container to use
+      container = "ul",               // Type of container to use
       hrefid    = "section";          // What to call links
 
   // Correct for off-by-one error on the limits.
@@ -28,9 +28,30 @@ function generateTOC(toc) {
   // Create the TOC container
   toc = toc.appendChild(document.createElement(container));
 
+  // Parent is body unless given
+  if(!parnt) parnt = document.body;
+
+
+
+  // Recursively get linear list of child nodes
+  function getChildren(parnt){
+    // Always return parent
+    var list = [parnt];
+    
+    // Convert from nodelist to array
+    var children = Array.prototype.slice.call(parnt.children);
+    for(var i=0; i<children.length; i++)
+      list = list.concat( getChildren( children[i] ));
+    return list;
+  };
+
+
   // Loop over all heading
-	for (var i = 0; i < document.body.childNodes.length; ++i) {
-    var node = document.body.childNodes[i];
+  var children = getChildren(parnt);
+  for(var i=0; i<children.length; i++){
+
+    // Load next node
+    var node = children[i];
 
     // If it's a heading...
     if (m = node.nodeName.match(rx)){
@@ -41,7 +62,7 @@ function generateTOC(toc) {
 
       // build section id for later use
       var section = levels[toclim[0]];
-      for( j=toclim[0]+1; j<=lv; j++ ) section += "." + levels[j];
+      for( var j=toclim[0]+1; j<=lv; j++ ) section += "." + levels[j];
 
       // If this is worthy of going in the TOC...
       if(lv >= toclim[0] && lv <= toclim[1]) {
@@ -51,7 +72,9 @@ function generateTOC(toc) {
 
         // Find the penultimate DOM item
         var t = toc;
-        for( j=0; j<((lv - toclim[0]) * 2 - 1); j++) t = t.lastChild;
+        for( j=0; j<((lv - toclim[0]) * 2 - 1); j++)
+          if(t.lastChild) t = t.lastChild;
+
 
         // Add a level of containers if necessary
         if ( levels[lv] == 1 && lv > toclim[0] ) t.appendChild(document.createElement(container));
@@ -69,9 +92,6 @@ function generateTOC(toc) {
       // Add numbering to headings if requested
       if(heading_nums && lv >= headlim[0] && lv <= headlim[1]) node.insertBefore(document.createTextNode(section + ". "), node.firstChild);
       node.id = hrefid + section;
-
     }
-
-	}
+  }
 }
-
